c4d0af4285c34b6b0fe80d73a88357c144e9ab46
Rik <161947286+RiquelmeLadislau@users.noreply.github.com>
2024-10-24 09:54:54 -0300

    eu

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const style = [
-    ['rock',
+    ['1 - rock',
         ['heavy metal','death metal','power metal','rock progressivo','Blues rock']],
-    ['pop'],
-    ['funk'],
-    ['trap'],
+    ['2 - pop'],
+    ['3 - funk'],
+    ['4 - trap'],
     
 ]
+let options =''
+options = prompt(`
+    Selecione seu estilo de musíca 
+    ${style}
+    `)
